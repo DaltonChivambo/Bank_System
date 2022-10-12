54498070ea699d419bb1b7dec2936f99e29e9e2e
dalton <daltonchivambo@gmail.com>
2022-10-12 16:47:19 +0200

    Implements Transition From SplashActivity to PasswordActivity

---
+++ app/src/main/java/com/skycoop/myapplication/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.skycoop.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Objects;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        //Create transitions in screens
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ActivityPassword();
+            }
+        },1500);
+    }
+
+
+    private void ActivityPassword(){
+        //Navegate to other activity (intent)
+
+        Intent intent = new Intent(SplashActivity.this,PasswordActivity.class);
+        startActivity(intent);
+    }
+}
