bfae6f601e92da70f92c7e2310a14aa0099b6bb7
dalton <daltonchivambo@gmail.com>
2022-10-15 09:41:13 +0200

    Implementig Events on PasswordActivity

--- app/src/main/java/com/skycoop/myapplication/MainActivity.java
+++ app/src/main/java/com/skycoop/myapplication/MainActivity.java
@@ -3,15 +3,20 @@ package com.skycoop.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
     }
 }
--- app/src/main/java/com/skycoop/myapplication/PasswordActivity.java
+++ app/src/main/java/com/skycoop/myapplication/PasswordActivity.java
@@ -3,16 +3,28 @@ package com.skycoop.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.Objects;
 
 public class PasswordActivity extends AppCompatActivity {
 
+    private EditText edit_password;
+    private Button bt_sign;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
-
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        edit_password = findViewById(R.id.edit_password);
+        bt_sign = findViewById(R.id.bt_sign);
+
+        bt_sign.setOnClickListener(view -> {
+            int registeredPassword = 111111;
+            int typedPassword;
+        });
     }
 }
